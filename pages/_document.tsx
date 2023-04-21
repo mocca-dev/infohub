1e7b0d2e4e799c948968a9a4885368aa07b39dd8
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-04-20 22:48:33 -0300

    Manifest.json and installable fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+        <meta name="theme-color" content="#1e1e1e" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Montserrat&display=optional: 200,400,600,700"
