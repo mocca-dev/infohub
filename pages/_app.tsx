8c5c31b9d51ed7883787a363433d2f4b22119f65
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-04-29 14:29:06 -0300

    Analytics addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Montserrat } from '@next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={montserrat.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
