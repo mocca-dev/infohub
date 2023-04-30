291e6355e0f588350e3d887faed5cded387c7fe1
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-04-30 02:06:00 -0300

    Header cleanup

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,14 +1,12 @@
 import styles from './header.module.css';
 import Logo from '../icons/logo';
 
-const Header = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.body}>
-        <Logo />
-      </div>
+const Header = () => (
+  <header className={styles.container}>
+    <div className={styles.body}>
+      <Logo />
     </div>
-  );
-};
+  </header>
+);
 
 export default Header;
