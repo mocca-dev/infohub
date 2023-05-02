7edc2f25be1f8375439b133c49634b6bd80419d7
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-05-02 01:44:40 -0300

    Dollar change color based on last value

--- components/card/dollarCard/dollarCard.tsx
+++ components/card/dollarCard/dollarCard.tsx
@@ -1,9 +1,41 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import styles from './dollarCard.module.css';
 import { DollarData } from '@/types/types';
+import Arrow from '@/components/icons/arrow';
 
-const DollarCard: FunctionComponent<DollarData> = ({ value }) => (
-  <div className={styles.cardValue}>{value}</div>
-);
+const DollarCard: FunctionComponent<DollarData> = ({ value }) => {
+  const [colorClass, setColorClass] = useState(styles.white);
+  const [isEqual, setIsEqual] = useState(false);
+
+  useEffect(() => {
+    const lastValue = localStorage.getItem('lastValue');
+    const currentValue = value?.replace('$', '');
+
+    if (lastValue) {
+      let lastValueNumber: number = parseInt(lastValue);
+      if (currentValue && parseInt(currentValue) > lastValueNumber) {
+        setColorClass(styles.green);
+        setIsEqual(false);
+      } else if (currentValue && parseInt(currentValue) === lastValueNumber) {
+        setColorClass(styles.white);
+        setIsEqual(true);
+      } else {
+        setColorClass(styles.red);
+        setIsEqual(false);
+      }
+    } else {
+      setColorClass(styles.green);
+    }
+    if (currentValue) localStorage.setItem('lastValue', currentValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={styles.cardValue + ' ' + colorClass}>
+      <span>{value}</span>
+      {isEqual ? null : <Arrow />}
+    </div>
+  );
+};
 
 export default DollarCard;
