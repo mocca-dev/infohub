06369520ee3101390928550e45f6389fc5875e1b
Damelanote2501 <nicolastonelli1406@gmail.com>
2023-10-18 13:43:26 -0300

    Holiday size fix

--- components/header/weather/weather.tsx
+++ components/header/weather/weather.tsx
@@ -7,19 +7,16 @@ type Props = {
   data: WeatherData;
 };
 
-const Weather: FunctionComponent<Props> = ({ data }) => {
-  console.log(data);
-  return (
-    <>
-      {data ? (
-        <span className={styles.weatherContainer}>
-          <span>Bahía Blanca,</span> {data?.temperature}°C
-          <Image src={data.icon} alt="weatherIcon" width="20" height="20" />
-          {/* {weather?.data.status} */}
-        </span>
-      ) : null}
-    </>
-  );
-};
+const Weather: FunctionComponent<Props> = ({ data }) => (
+  <>
+    {data ? (
+      <span className={styles.weatherContainer}>
+        <span>Bahía Blanca,</span> {data?.temperature}°C
+        <Image src={data.icon} alt="weatherIcon" width="20" height="20" />
+        {/* {weather?.data.status} */}
+      </span>
+    ) : null}
+  </>
+);
 
 export default Weather;
